f201341b6076c1cdf11f56cac386900e36a32011
Hunter Sarnelli <huntersarnelli@gmail.com>
2025-08-25 12:36:39 -0400

    Initial Commit

---
+++ Load Restraint Pro/Load_Restraint_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  Load_Restraint_ProApp.swift
+//  Load Restraint Pro
+//
+//  Created by Hunter Sarnelli on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Load_Restraint_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Load Restraint ProTests/Load_Restraint_ProTests.swift
@@ -0,0 +1,17 @@
+//
+//  Load_Restraint_ProTests.swift
+//  Load Restraint ProTests
+//
+//  Created by Hunter Sarnelli on 8/25/25.
+//
+
+import Testing
+@testable import Load_Restraint_Pro
+
+struct Load_Restraint_ProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
